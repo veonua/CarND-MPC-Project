c3859657e023c6562b32862fd412a26f54814377
Andrew <veon.ua@gmail.com>
2017-12-10 00:01:10 +0200

    initial builds but does not work

--- src/main.cpp
+++ src/main.cpp
@@ -14,31 +14,33 @@ using json = nlohmann::json;
 
 // For converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
+
 double deg2rad(double x) { return x * pi() / 180; }
+
 double rad2deg(double x) { return x * 180 / pi(); }
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
 string hasData(string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.rfind("}]");
-  if (found_null != string::npos) {
+    auto found_null = s.find("null");
+    auto b1 = s.find_first_of("[");
+    auto b2 = s.rfind("}]");
+    if (found_null != string::npos) {
+        return "";
+    } else if (b1 != string::npos && b2 != string::npos) {
+        return s.substr(b1, b2 - b1 + 2);
+    }
     return "";
-  } else if (b1 != string::npos && b2 != string::npos) {
-    return s.substr(b1, b2 - b1 + 2);
-  }
-  return "";
 }
 
 // Evaluate a polynomial.
 double polyeval(Eigen::VectorXd coeffs, double x) {
-  double result = 0.0;
-  for (int i = 0; i < coeffs.size(); i++) {
-    result += coeffs[i] * pow(x, i);
-  }
-  return result;
+    double result = 0.0;
+    for (int i = 0; i < coeffs.size(); i++) {
+        result += coeffs[i] * pow(x, i);
+    }
+    return result;
 }
 
 // Fit a polynomial.
@@ -46,140 +48,185 @@ double polyeval(Eigen::VectorXd coeffs, double x) {
 // https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
 Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
                         int order) {
-  assert(xvals.size() == yvals.size());
-  assert(order >= 1 && order <= xvals.size() - 1);
-  Eigen::MatrixXd A(xvals.size(), order + 1);
+    assert(xvals.size() == yvals.size());
+    assert(order >= 1 && order <= xvals.size() - 1);
+    Eigen::MatrixXd A(xvals.size(), order + 1);
 
-  for (int i = 0; i < xvals.size(); i++) {
-    A(i, 0) = 1.0;
-  }
-
-  for (int j = 0; j < xvals.size(); j++) {
-    for (int i = 0; i < order; i++) {
-      A(j, i + 1) = A(j, i) * xvals(j);
+    for (int i = 0; i < xvals.size(); i++) {
+        A(i, 0) = 1.0;
     }
-  }
 
-  auto Q = A.householderQr();
-  auto result = Q.solve(yvals);
-  return result;
+    for (int j = 0; j < xvals.size(); j++) {
+        for (int i = 0; i < order; i++) {
+            A(j, i + 1) = A(j, i) * xvals(j);
+        }
+    }
+
+    auto Q = A.householderQr();
+    auto result = Q.solve(yvals);
+    return result;
+}
+
+double mph_to_mps(double mph_) {
+    return mph_*0.44704;  //1 mph = 0.44704 m/s
 }
 
 int main() {
-  uWS::Hub h;
+    uWS::Hub h;
 
-  // MPC is initialized here!
-  MPC mpc;
+    // MPC is initialized here!
+    MPC mpc;
+    Eigen::VectorXd state(6);
 
-  h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                     uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event.
-    // The 4 signifies a websocket message
-    // The 2 signifies a websocket event
-    string sdata = string(data).substr(0, length);
-    cout << sdata << endl;
-    if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2') {
-      string s = hasData(sdata);
-      if (s != "") {
-        auto j = json::parse(s);
-        string event = j[0].get<string>();
-        if (event == "telemetry") {
-          // j[1] is the data JSON object
-          vector<double> ptsx = j[1]["ptsx"];
-          vector<double> ptsy = j[1]["ptsy"];
-          double px = j[1]["x"];
-          double py = j[1]["y"];
-          double psi = j[1]["psi"];
-          double v = j[1]["speed"];
+    h.onMessage([&mpc, &state](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+                               uWS::OpCode opCode) {
+        // "42" at the start of the message means there's a websocket message event.
+        // The 4 signifies a websocket message
+        // The 2 signifies a websocket event
+        string sdata = string(data).substr(0, length);
+        cout << sdata << endl;
+        if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2') {
+            string s = hasData(sdata);
+            if (s != "") {
+                auto j = json::parse(s);
+                string event = j[0].get<string>();
+                if (event == "telemetry") {
+                    // j[1] is the data JSON object
+                    vector<double> ptsx = j[1]["ptsx"];
+                    vector<double> ptsy = j[1]["ptsy"];
+                    double px = j[1]["x"];
+                    double py = j[1]["y"];
+                    double psi = j[1]["psi"];
+                    double v = j[1]["speed"];
 
-          /*
-          * TODO: Calculate steering angle and throttle using MPC.
-          *
-          * Both are in between [-1, 1].
-          *
-          */
-          double steer_value;
-          double throttle_value;
+                    // Velocity, as reported by the simulator, is in mph,
+                    // while the waypoints are reported in meters.
+                    v = mph_to_mps(v);
 
-          json msgJson;
-          // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
-          // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = throttle_value;
+                    /////
+                    auto len = ptsx.size();
+                    Eigen::VectorXd wp_x(len);
+                    Eigen::VectorXd wp_y(len);
+                    auto cosi = cos(psi);
+                    auto sini = sin(psi);
 
-          //Display the MPC predicted trajectory 
-          vector<double> mpc_x_vals;
-          vector<double> mpc_y_vals;
+                    for (size_t i = 0; i < len; i++) {
+                        double x_t = ptsx[i] - px,  // x, y translated
+                                y_t = ptsy[i] - py;
+                        wp_x[i] =  x_t * cosi + y_t * sini;
+                        wp_y[i] = -x_t * sini + y_t * cosi;
+                    }
+                    // fit polynimal to received waypoints
+                    Eigen::VectorXd wpts_coeff = polyfit(wp_x, wp_y, 2);
 
-          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
-          // the points in the simulator are connected by a Green line
+                    double steer_value;
+                    double throttle_value;
 
-          msgJson["mpc_x"] = mpc_x_vals;
-          msgJson["mpc_y"] = mpc_y_vals;
+                    // reset vehicle position in accordance to transformation
+                    px = 0.0;
+                    py = 0.0;
+                    psi = 0;
 
-          //Display the waypoints/reference line
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
+                    // compute cte
+                    double cte = wpts_coeff[0];
+                    double epsi = -atan(wpts_coeff[1]);
 
-          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
-          // the points in the simulator are connected by a Yellow line
+                    state << px, py, psi, v, cte, epsi;
 
-          msgJson["next_x"] = next_x_vals;
-          msgJson["next_y"] = next_y_vals;
+                    vector<double> solution = mpc.Solve(state, wpts_coeff);
+                    auto num_pts = (solution.size() - 2) / 2;
+
+                    steer_value = -solution[0];  // minus sign because axis y is inverted in simulator
+                    throttle_value = solution[1];  // minus sign because axis y is inverted in simulator
+
+                    vector<double> mpc_x_vals(num_pts);
+                    vector<double> mpc_y_vals(num_pts);
+
+                    for (size_t i = 0; i < num_pts; i++) {
+                        mpc_x_vals[i] = solution[i + 2];
+                        mpc_y_vals[i] = solution[num_pts + i + 2];
+                    }
+
+                    json msgJson;
+                    // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
+                    // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
+                    msgJson["steering_angle"] = steer_value;
+                    msgJson["throttle"] = throttle_value;
+
+                    //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
+                    // the points in the simulator are connected by a Green line
+
+                    msgJson["mpc_x"] = mpc_x_vals;
+                    msgJson["mpc_y"] = mpc_y_vals;
+
+                    //Display the waypoints/reference line
+                    vector<double> next_x_vals;
+                    vector<double> next_y_vals;
+
+                    for (int i = 0; i < 15; i++) {
+                        next_x_vals.push_back(i*5);
+                        next_y_vals.push_back(polyeval(wpts_coeff, i*5));
+                    }
+
+                    //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
+                    // the points in the simulator are connected by a Yellow line
+
+                    msgJson["next_x"] = next_x_vals;
+                    msgJson["next_y"] = next_y_vals;
 
 
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
-          // Latency
-          // The purpose is to mimic real driving conditions where
-          // the car does actuate the commands instantly.
-          //
-          // Feel free to play around with this value but should be to drive
-          // around the track with 100ms latency.
-          //
-          // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
-          // SUBMITTING.
-          this_thread::sleep_for(chrono::milliseconds(100));
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+                    auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+                    std::cout << msg << std::endl;
+                    // Latency
+                    // The purpose is to mimic real driving conditions where
+                    // the car does actuate the commands instantly.
+                    //
+                    // Feel free to play around with this value but should be to drive
+                    // around the track with 100ms latency.
+                    //
+                    // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
+                    // SUBMITTING.
+                    this_thread::sleep_for(chrono::milliseconds(100));
+                    ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+                }
+            } else {
+                // Manual driving
+                std::string msg = "42[\"manual\",{}]";
+                ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+            }
         }
-      } else {
-        // Manual driving
-        std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-      }
-    }
-  });
+    });
 
-  // We don't need this since we're not using HTTP but if it's removed the
-  // program
-  // doesn't compile :-(
-  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
-                     size_t, size_t) {
-    const std::string s = "<h1>Hello world!</h1>";
-    if (req.getUrl().valueLength == 1) {
-      res->end(s.data(), s.length());
+    // We don't need this since we're not using HTTP but if it's removed the
+    // program
+    // doesn't compile :-(
+    h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
+                       size_t, size_t) {
+        const std::string s = "<h1>Hello world!</h1>";
+        if (req.getUrl().valueLength == 1) {
+            res->end(s.data(), s.length());
+        } else {
+            // i guess this should be done more gracefully?
+            res->end(nullptr, 0);
+        }
+    });
+
+    h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+        std::cout << "Connected!!!" << std::endl;
+    });
+
+    h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+                           char *message, size_t length) {
+        ws.close();
+        std::cout << "Disconnected" << std::endl;
+    });
+
+    int port = 4567;
+    if (h.listen(port)) {
+        std::cout << "Listening to port " << port << std::endl;
     } else {
-      // i guess this should be done more gracefully?
-      res->end(nullptr, 0);
+        std::cerr << "Failed to listen to port" << std::endl;
+        return -1;
     }
-  });
-
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
-  });
-
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
-                         char *message, size_t length) {
-    ws.close();
-    std::cout << "Disconnected" << std::endl;
-  });
-
-  int port = 4567;
-  if (h.listen(port)) {
-    std::cout << "Listening to port " << port << std::endl;
-  } else {
-    std::cerr << "Failed to listen to port" << std::endl;
-    return -1;
-  }
-  h.run();
+    h.run();
 }
